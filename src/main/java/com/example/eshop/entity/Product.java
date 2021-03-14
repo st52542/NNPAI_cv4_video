281f0d4e8a696804d2b0d3f38305012a1843b9b0
st52542 <43936282+st52542@users.noreply.github.com>
2021-03-14 03:25:44 +0100

    init

---
+++ src/main/java/com/example/eshop/entity/Product.java
@@ -0,0 +1,44 @@
+package com.example.eshop.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+
+
+    @OneToMany(mappedBy = "id",fetch = FetchType.LAZY)
+    private Set<OrderHasProduct> productInOrders;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<OrderHasProduct> getProductInOrders() {
+        return productInOrders;
+    }
+
+    public void setProductInOrders(Set<OrderHasProduct> productInOrders) {
+        this.productInOrders = productInOrders;
+    }
+}
